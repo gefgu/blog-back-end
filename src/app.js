5d5d6bc03d5f2979f66bd0724f7a27297b17038a
gefgu <gefgu@hotmail.com>
2022-07-17 19:41:38 -0300

    Add connection to MongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoDb = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qxb9y.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => res.json({ message: "abc" }));
 
