1336ee046ddee85f612941c87ecd30980f1ae730
gefgu <gefgu@hotmail.com>
2022-07-18 15:11:19 -0300

    Add PUT method for /posts/:postId

--- src/routes/post.js
+++ src/routes/post.js
@@ -69,4 +69,58 @@ router.get("/:postId", (req, res, next) => {
     });
 });
 
+router.put("/:postId", [
+  body("title", "Title must be specified").trim().isLength({ min: 1 }).escape(),
+  body("content", "content must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("creationDate", "creationDate must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("author", "author must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  check("author").custom((value, { req }) => {
+    return new Promise((resolve, reject) => {
+      req.context.models.User.findById(value, (err, user) => {
+        if (user) {
+          resolve();
+        } else {
+          reject();
+        }
+      });
+    });
+  }),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.array().length > 0) {
+      res.json(errors);
+      return;
+    }
+
+    const post = new req.context.models.Post({
+      _id: req.params.postId,
+      title: req.body.title,
+      content: req.body.content,
+      creationDate: req.body.creationDate,
+      publishDate: req.body.publishDate || null,
+      author: req.body.author,
+    });
+
+    req.context.models.Post.findByIdAndUpdate(
+      req.params.postId,
+      post,
+      {},
+      function (err) {
+        if (err) return next(err);
+
+        res.json({ message: "Post UPDATED with success!" });
+      }
+    );
+  },
+]);
+
 module.exports = router;
