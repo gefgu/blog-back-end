29310608cb1b25d7e43447a052ac3d0e7a4b5800
gefgu <gefgu@hotmail.com>
2022-07-18 15:55:44 -0300

    Add create user method

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,8 +3,40 @@ const { body, check, validationResult } = require("express-validator");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.json({ message: "Hello World!" });
-});
+router.post("/", [
+  body("username", "username must be specified").trim().isLength().escape(),
+  body("username", "username already in use").custom((value, { req }) => {
+    return new Promise((resolve, reject) => {
+      req.context.models.User.findOne({ username: value }, (err, user) => {
+        if (user) {
+          reject();
+        } else {
+          resolve();
+        }
+      });
+    });
+  }),
+  body("password", "password must be bcrypt hashed value").trim().isLength(60), // Length of Bcrypt hashed password
+  body("admin", "admin must be specified").isBoolean(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.array().length > 0) {
+      res.json(errors);
+      return;
+    }
+
+    const user = new req.context.models.User({
+      username: req.body.username,
+      password: req.body.password,
+      admin: req.body.admin,
+    });
+
+    user.save(function (err) {
+      if (err) return next(err);
+
+      res.json({ message: "USER CREATED WITH SUCCESS!", user });
+    });
+  },
+]);
 
 module.exports = router;
