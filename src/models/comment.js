26e423d8a2c37efeae13b1ca482b9692411b9de4
gefgu <gefgu@hotmail.com>
2022-07-19 09:31:16 -0300

    Fix missing required fields in models

--- src/models/comment.js
+++ src/models/comment.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const CommentSchema = new Schema({
   content: { type: String, required: true },
   creationDate: { type: Date, required: true, default: Date.now },
-  post: { type: Schema.Types.ObjectId, ref: "Post" },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
